8df96b226aec1c445c7cc1ea0fe3bdbae4172040
Daniel M <daniel.milli@hotmail.com>
2021-10-21 20:36:03 -0300

    JavaScript code added + HTML modified

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #NLW Heat
 
-1. Learning Front-End coding with @rocketseat
+1. Virtual Badge for the DoWhile 2021 online event
+2. Learning Front-End coding with @rocketseat
---
+++ main.js
@@ -0,0 +1,31 @@
+const socialLinks = {
+  github: 'gbdanielbr',
+  instagram: '_moraesdaniel',
+  linkedin: 'in/danielmcm'
+}
+
+function changeSocialLinks() {
+  for (let li of socialIcons.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${socialLinks[social]}`
+  }
+}
+
+changeSocialLinks()
+
+function getGitProfileInfo() {
+  const url = `https://api.github.com/users/${socialLinks.github}`
+
+  fetch(url)
+    .then(answer => answer.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userGitLink.href = data.html_url
+      userGitId.textContent = data.login
+      userImg.src = data.avatar_url
+    })
+}
+
+getGitProfileInfo()
